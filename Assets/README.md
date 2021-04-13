c8cbbb19782f60fcc3c6c2a783a50798e048781c
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2021-04-13 18:35:15 -0500

    Updated code that displays current day weather

--- Assets/README.md
+++ Assets/README.md
@@ -18,6 +18,7 @@ SO THAT I can plan a trip accordingly
 
 ```
 GIVEN a weather dashboard with form inputs
+
 WHEN I search for a city
 THEN I am presented with current and future conditions for that city and that city is added to the search history
 
--- script.js
+++ script.js
@@ -1,19 +1,61 @@
 var key = 'a4b00bad833f4e991e075f77de4479fd';
-var cityName = document.getElementById('cityName').value;
-var currentUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Chicago&appid=${key}`;
-var openUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=a4b00bad833f4e991e075f77de4479fd';
 var previousCity = [];
 var cityButton = document.getElementById('search');
+var cityName1HTML = document.getElementById('cityName1');
+var tempHTML = document.getElementById('temp');
+var humidHTML = document.getElementById('humid');
+var windHTML = document.getElementById('wind');
+var uvHTML = document.getElementById('uv');
 
-function getApi(){
+
+function getCity(){
+    var cityName = document.getElementById('cityName').value;
+    var currentUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${key}`;
+  
+    var units = '&units=imperial';
     fetch(currentUrl)
-        .then(function(getloc) {
-            return getloc.json();
-        })
-        .then(function(data){
-            console.log('Weather');
-        })
-}
+        .then(function(rawData) {
+            return rawData.json();
+        }
+        )
+        .then(function(data)
+            {
+                var openUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.city.coord.lat}&lon=${data.city.coord.lon}&units=imperial&appid=${key}`;
+                
+                fetch(openUrl) 
+                    .then(function(weatherData) {
+                            return weatherData.json();
+                    }
+                    )
+                    .then(function(allweatherData)
+                        {
+                            var cityName1 = document.createElement('h3');
+                            var temp = document.createElement('h4');
+                            var humid = document.createElement('h4');
+                            var wind = document.createElement('h4');
+                            var uv = document.createElement('h4');
+                                cityName1.textContent = data.city.name;
+                                temp.textContent = allweatherData.daily[0].temp.day;
+                                humid.textContent = allweatherData.daily[0].humidity;
+                                wind.textContent = allweatherData.daily[0].wind_speed;
+                                uv.textContent = allweatherData.daily[0].uvi;
 
-cityButton.addEventListener('click', getApi);
+                                //append child
+                                cityName1HTML.appendChild(cityName1);
+                                tempHTML.appendChild(temp);
+                                humidHTML.appendChild(humid);
+                                windHTML.appendChild(wind);
+                                uvHTML.appendChild(uv);
+
+
+
+
+                                //for loop for remainder of days 
+                        }
+                    )
+            }
+        )
+};
+
+cityButton.addEventListener('click', getCity);
 
